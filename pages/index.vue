087b3f5af19d873266b59b9b7f2995cfb3da00db
schraderweb <73207923+schraderweb@users.noreply.github.com>
2024-12-02 11:41:44 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,289 @@
-<template>
-  <div>
-<iframe src="https://gleeful-meerkat-e95895.netlify.app/" style="height: 100vh; width:100%;"></iframe>
-</div>
-</template>
- 
+<!DOCTYPE html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Well Within Farm Sanctuary | Coming Soon</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.png" />
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="/css/normalize.css" />
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="/css/main.css" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="/css/fontawesome-all.min.css" />
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="/css/animate.min.css" />
+    <!-- FlatIcon CSS -->
+    <link rel="stylesheet" href="/font/flaticon.css" />
+    <!-- Nivo Slider CSS -->
+<!--    <link rel="stylesheet" href="/vendor/slider/css/nivo-slider.css" />-->
+    <!-- Owl Carousel js -->
+    <link rel="stylesheet" href="/vendor/OwlCarousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="/vendor/OwlCarousel/owl.theme.default.min.css" />
+    <!-- Magnific Popup CSS -->
+    <link rel="stylesheet" href="/css/magnific-popup.css" />
+    <!-- Select 2 CSS -->
+    <link rel="stylesheet" href="/css/select2.min.css" />
+    <!-- MeanMenu CSS -->
+    <link rel="stylesheet" href="/css/meanmenu.min.css" />
+    <!-- new page css -->
+    <link rel="stylesheet" href="/css/additional.css" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/style.css" />
+    <!-- Modernize js -->
+    <script src="/js/modernizr-3.7.1.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+
+<body class="sticky-header">
+    <!-- ScrollUp Start Here -->
+    <a href="#wrapper" data-type="section-switch" class="scrollup">
+        <i class="fas fa-angle-double-up"></i>
+    </a>
+    <!-- ScrollUp End Here -->
+    <!-- Preloader Start Here -->
+    <div id="preloader"></div>
+    <!-- Preloader End Here -->
+    <div id="wrapper" class="wrapper">
+        <!-- Add your site or application content here -->
+        <!-- Header Area Start Here -->
+<!--           <header class="header">
+       <div id="header-topbar" class="pd-b-25 pd-t-35">
+           <div class="container">
+               <div class="row align-items-center">
+                   <div class="col-lg-5">
+                       <div class="header-topbar-layout2">
+                           <div class="phone-number">
+                               <i class="flaticon-call-answer"></i> (616) 481-2097
+                           </div>
+                           <span>Call today for a free test.</span>
+                       </div>
+                   </div>
+                   <div class="col-lg-2">
+                       <div class="logo-area">
+                           <a href="/" class="temp-logo">
+                               <img src="/images/logo-main.jpg" alt="logo" class="img-fluid" width="150" />
+                           </a>
+                       </div>
+                   </div>
+                   <div class="col-lg-5 d-flex justify-content-end">
+                       <div class="header-action-layout1">
+                           <ul>
+                               <li class="header-action-btn">
+                                   <a href="/contact-us" class="item-btn">GET A FREE ESTIMATE</a>
+                               </li>
+                           </ul>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div id="rt-sticky-placeholder"></div>
+       <div id="header-menu" class="header-menu menu-layout3 bg-accent">
+           <div class="container">
+               <div class="row d-flex align-items-center">
+                   <div class="col-lg-12 d-flex justify-content-center possition-static">
+                       <nav id="dropdown" class="template-main-menu">
+                           <ul>
+                               <li>
+                                   <a href="/about-us">ABOUT US</a>
+                               </li>
+                               <li>
+                                   <a href="/service-area">Service Area</a>
+                               </li>
+                               <li>
+                                   <a href="/gallery">Gallery</a>
+                               </li>
+                               <li>
+                                   <a href="/our-services.html">OUR SERVICES</a>
+                                   <ul class="dropdown-menu-col-1">
+                                       <li>
+                                           <a href="/house-washing">House Washing</a>
+                                       </li>
+                                       <li>
+                                           <a href="/roof-cleaning">Roof Cleaning</a>
+                                       </li>
+                                       <li>
+                                           <a href="/gutter-cleaning">Gutter Cleaning</a>
+                                       </li>
+                                       <li>
+                                           <a href="/driveway-cleaning">Driveway Cleaning</a>
+                                       </li>
+                                       <li>
+                                           <a href="/paver-cleaning">Paver Cleaning</a>
+                                       </li>
+                                       <li>
+                                           <a href="/sidewalk-cleaning">Sidewalk Cleaning</a>
+                                       </li>
+                                       <li>
+                                           <a href="/fence-cleaning">Fence Cleaning</a>
+                                       </li>
+                                       <li>
+                                           <a href="/window-cleaning">Window Cleaning</a>
+                                       </li>
+                                   </ul>
+                               </li>
+                               <li>
+                                   <a href="/testimonials">Testimonials</a>
+                               </li>
+                               <li>
+                                   <a href="/contact-us">CONTACT US</a>
+                               </li>
+                           </ul>
+                       </nav>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </header>
+-->
+        <!-- Header Area End Here -->
+
+        
+<section>
+    <div class="container-fluid text-black d-none d-md-block ">
+        <div class="row min-vh-100" style="background-image: url('images/coming soon.jpg'); background-size: cover;background-position: center;  background-repeat: no-repeat;">
+            <div class="col-xl-5 col-lg-7 col-md-7 p-5 p-lg-4 d-flex align-items-center" style="background-color: rgba(255, 255, 255, 0.5); height: 91vh !important;">
+                <div class="mt-5 pl-5 ml-5">
+                    <h1 class="text-uppercase">Coming Soon</h1>
+                    <p> <img src="images/logo.png" alt="companylogo"></p>
+                    <h2>Well Within Farm Sanctuary</h2>
+                    <p class="lead display-4" style="color: #0b0b0b">
+                        Call Us Today At (231) 252-6405
+                    </p>
+<!--                    extra description goes down in p-->
+<!--                    <p class="social"><a href="#" class="external facebook"><i class="fab fa-facebook-f"></i></a><a href="#" class="external youtube"><i class="fab fa-youtube"></i></a><a href="#" class="external twitter"><i class="fab fa-twitter"></i></a><a href="#" title="" class="external instagram"><i class="fab fa-instagram"></i></a><a href="#" class="email"><i class="fa fa-envelope"> </i></a></p>-->
+<!--                    <p class="credit text-white">&copy; 2019 Your company | Template by <a href="#" class="external text-reset">Bootstrapious</a>  </p>-->
+                    <!-- Please do not remove the backlink to bootstrapious unless your Attribution-Free License @ https://bootstrapious.com/attribution-free-license. It is part of the license conditions. Thanks for understanding :) -->
+                </div>
+            </div>
+            <div  class="col-xl-7 col-lg-5 col-md-5 intro-right">
+
+            </div>
+            <div class="container-fluid " style="background-color: rgba(0, 0, 0, 0.7); height: 96px; font-size: 2.0em">
+                <footer class="row align-items-center justify-content-center" >
+                    <p class="py-5 text-white pb-0 mb-0">
+                        &copy; 2014-
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        Well Within Farm Sanctuary | Website developed & hosted by
+                        <a href="https://schrader.co"  class="text-primary" target="_blank">Schrader Web Solutions</a>
+                    </p>
+                </footer>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid text-black d-xs-block d-sm-none">
+        <div class="row  min-vh-100" >
+            <div class="col-xl-4 col-lg-5 col-md-4 col-sm-12 p-5 p-lg-4 d-flex align-items-center text-center bg-light" style=" ">
+                <div class="mt-5">
+                    <h1 class="text-uppercase mb-3">Coming Soon</h1>
+                    <p> <img src="images/logo.png" alt="companylogo"></p>
+                    <h2 class="mb-4">Well Within Farm Sanctuary</h2>
+                    <p class="lead display-4 text-white mt-3 " style="width: 360px">
+                        <a class="btn btn-success p-2 m-3 w-100" href="tel:+12312526405" style="font-size: 0.8em; border-radius: 12px;">
+                            <i class="fa fa-phone border border-white rounded-circle p-3 mr-3" aria-hidden="true" style=""></i>
+                            <span class="ml-3 font-weight-bolder" >Click To Call Us </span>
+                        </a>
+
+                    </p>
+                    <!--                    extra description goes down in p-->
+<!--                    <p class="social"><a href="#" class="external facebook"><i class="fab fa-facebook-f"></i></a><a href="#" class="external youtube"><i class="fab fa-youtube"></i></a><a href="#" class="external twitter"><i class="fab fa-twitter"></i></a><a href="#" title="" class="external instagram"><i class="fab fa-instagram"></i></a><a href="#" class="email"><i class="fa fa-envelope"> </i></a></p>-->
+                    <!--                    <p class="credit text-white">&copy; 2019 Your company | Template by <a href="#" class="external text-reset">Bootstrapious</a>  </p>-->
+                    <!-- Please do not remove the backlink to bootstrapious unless your Attribution-Free License @ https://bootstrapious.com/attribution-free-license. It is part of the license conditions. Thanks for understanding :) -->
+                </div>
+            </div>
+            <div  class="col-xl-8 col-lg-7 col-md-8 col-sm-0 intro-right" style="background-image: url('images/coming soon.jpg'); background-size: cover;background-position: center;  background-repeat: no-repeat;">
+
+            </div>
+            <div class="container-fluid " style="background-color: rgba(0, 0, 0, 0.7); height: 96px; font-size: 2.0em">
+                <footer class="row align-items-center justify-content-center" >
+                    <p class="py-5 text-white p-2 pb-0 mb-0 text-center">
+                        &copy; 2014-
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        Well Within Farm Sanctuary | Website developed & hosted by
+                        <a href="https://schrader.co"  class="text-primary" target="_blank">Schrader Web Solutions</a>
+                    </p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+        <!-- Footer Area Start Here -->
+        <!-- Project Area Start Here -->
+
+<!--<footer class="footer-wrap-layout1" style="min-height: 5vh !important;">-->
+<!--    <div class="footer-bottom-wrap-layout1">-->
+<!--        <div class="container">-->
+<!--            <div class="copyright">-->
+<!--                Copyright © 2014- 2023 Anchor Pro Wash | Website developed &-->
+<!--                hosted by-->
+<!--                <a href="https://schrader.co/"> Schrader Web Solutions </a>-->
+<!--            </div>-->
+
+<!--            <div class="copyright">-->
+<!--                <a href="#">Privacy Policy</a>-->
+<!--                |-->
+<!--                <a href="#">Terms Conditions</a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</footer>-->
+
+
+
+        <!-- Footer Area End Here -->
+    </div>
+    <!-- jquery-->
+    <script src="/js/jquery-3.3.1.min.js"></script>
+    <!-- Plugins js -->
+    <script src="/js/plugins.js"></script>
+    <!-- Popper js -->
+    <script src="/js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- MeanMenu js -->
+    <script src="/js/jquery.meanmenu.min.js"></script>
+    <!-- Isotop js -->
+    <script src="/js/isotope.pkgd.min.js"></script>
+    <!-- ImagesLoaded js -->
+    <script src="/js/imagesloaded.pkgd.min.js"></script>
+    <!-- Nivo Slider js -->
+    <script src="/vendor/slider/js/jquery.nivo.slider.js"></script>
+    <script src="/vendor/slider/home.js"></script>
+    <!-- Owl Carousel js -->
+    <script src="/vendor/OwlCarousel/owl.carousel.min.js"></script>
+    <!-- Counter Up js -->
+    <script src="/js/jquery.counterup.min.js"></script>
+    <!-- Waypoints js -->
+    <script src="/js/waypoints.min.js"></script>
+    <!-- Magnific Popup js -->
+    <script src="/js/jquery.magnific-popup.min.js"></script>
+    <!-- Select 2 js -->
+    <script src="/js/select2.min.js"></script>
+    <!-- Validator js -->
+    <script src="/js/validator.min.js"></script>
+    <!-- Parallax js -->
+    <script src="/js/parallax.min.js"></script>
+    <!-- Main js -->
+    <script src="/js/main.js"></script>
+
+</body>
+
+</html>
